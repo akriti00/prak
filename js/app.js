5553c8fff7b195aa262f9dfe1a70b84417963b1c
akriti00 <86553040+akriti00@users.noreply.github.com>
2022-11-17 16:47:11 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,149 @@
+const container = document.querySelector(".container");
+const coffees = [
+  {
+    name: "Perspiciatis",
+    image: "images/coffee1.jpg"
+  },
+  {
+    name: "Voluptatem",
+    image: "images/coffee2.jpg"
+  },
+  {
+    name: "Explicabo",
+    image: "images/coffee3.jpg"
+  },
+  {
+    name: "Rchitecto",
+    image: "images/coffee4.jpg"
+  },
+  {
+    name: " Beatae",
+    image: "images/coffee5.jpg"
+  },
+  {
+    name: " Vitae",
+    image: "images/coffee6.jpg"
+  },
+  {
+    name: "Inventore",
+    image: "images/coffee7.jpg"
+  },
+  {
+    name: "Veritatis",
+    image: "images/coffee8.jpg"
+  },
+  {
+    name: "Accusantium",
+    image: "images/coffee9.jpg"
+  }
+];
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+              <div class="card">
+                <img class="card--avatar" src=${image} />
+                <h1 class="card--title">${name}</h1>
+                <a class="card--link" href="#">Taste</a>
+              </div>
+              `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err));
+  });
+}
+
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
+    });
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
+}
+if ('LinearAccelerationSensor' in window && 'Gyroscope' in window) {
+  document.getElementById('moApi').innerHTML = 'Generic Sensor API';
+  
+  let lastReadingTimestamp;
+  let accelerometer = new LinearAccelerationSensor();
+  accelerometer.addEventListener('reading', e => {
+    if (lastReadingTimestamp) {
+      intervalHandler(Math.round(accelerometer.timestamp - lastReadingTimestamp));
+    }
+    lastReadingTimestamp = accelerometer.timestamp
+    accelerationHandler(accelerometer, 'moAccel');
+  });
+  accelerometer.start();
+  
+  if ('GravitySensor' in window) {
+    let gravity = new GravitySensor();
+    gravity.addEventListener('reading', e => accelerationHandler(gravity, 'moAccelGrav'));
+    gravity.start();
+  }
+  
+  let gyroscope = new Gyroscope();
+  gyroscope.addEventListener('reading', e => rotationHandler({
+    alpha: gyroscope.x,
+    beta: gyroscope.y,
+    gamma: gyroscope.z
+  }));
+  gyroscope.start();
+  
+} else if ('DeviceMotionEvent' in window) {
+  document.getElementById('moApi').innerHTML = 'Device Motion API';
+  
+  var onDeviceMotion = function (eventData) {
+    accelerationHandler(eventData.acceleration, 'moAccel');
+    accelerationHandler(eventData.accelerationIncludingGravity, 'moAccelGrav');
+    rotationHandler(eventData.rotationRate);
+    intervalHandler(eventData.interval);
+  }
+  
+  window.addEventListener('devicemotion', onDeviceMotion, false);
+} else {
+  document.getElementById('moApi').innerHTML = 'No Accelerometer & Gyroscope API available';
+}
+
+function accelerationHandler(acceleration, targetId) {
+  var info, xyz = "[X, Y, Z]";
+
+  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
+  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
+  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
+  document.getElementById(targetId).innerHTML = info;
+}
+
+function rotationHandler(rotation) {
+  var info, xyz = "[X, Y, Z]";
+
+  info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
+  info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
+  info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
+  document.getElementById("moRotation").innerHTML = info;
+}
+
+function intervalHandler(interval) {
+  document.getElementById("moInterval").innerHTML = interval;
+}
